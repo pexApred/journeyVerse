a4467601795bd280058207e8204e995d4d7281a8
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-09 16:55:05 -0400

    pushing app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {  ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+// MW - Import ApolloClient, ApolloProvider, InMemoryCache, createHttpLink
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
+// MW - Import setContext
 import { setContext } from '@apollo/client/link/context';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // TODO: Import pages here
 import Navbar from './components/NavBar';
-import LandingPages from './pages/LandingPages';
 import LoginPage from './pages/Login';
 
+// MW - Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+// MW - Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
+  // MW - get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
+  // MW - return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -22,6 +28,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
+  // MW - Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -30,12 +37,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+        <>
           {/* <Navbar /> */}
           <Routes>
             <Route path='/login' element={<LoginPage/>}> 
             </Route>
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Routes>
+        </>
       </Router>
     </ApolloProvider>
   );
