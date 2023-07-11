fe71f735d8fdf324c038940683a07e775e4eaed8
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-07-11 16:29:26 -0400

    Footer.js styling, Footer.css, src index.js imported bootstrap, main index.js alinked css and boostrap stylesheets

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // MW - Import ApolloClient, ApolloProvider, InMemoryCache, createHttpLink
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
 // MW - Import setContext
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>This is the footer.</p>
+    <footer className='footer fixed-bottom mt-auto py-3'>
+      <Container>
+        <Row>
+          <Col className='text-center text-sm-left'>
+          <img src="/client/public/journeyverse-logo.png" alt="Logo" className="logo" />
+          </Col>
+          <Col xs={12} sm={4} className="text-center">
+            <p className="footer-text">JourneyVerse ©2023</p>
+          </Col>
+          <Col xs={12} sm={4} className="text-center text-sm-right">
+            <a href="https://github.com/pexApred/journeyVerse" className="footer-link">GitHub</a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
