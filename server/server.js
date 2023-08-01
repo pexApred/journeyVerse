344425f18b01de1588ece5381040306967141918
pexApred <120075705+pexApred@users.noreply.github.com>
2023-08-01 14:55:21 -0400

    Added a catch all route for server

--- server/server.js
+++ server/server.js
@@ -35,6 +35,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+// Catch-all route handler
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // app.use(routes);
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
